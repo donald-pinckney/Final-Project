0f71f07c543dee8af3a2e66f3e45bffb47ef4721
LTaylor <darting.thoughts@gmail.com>
2016-10-20 20:19:22 -0700

    Update KonaneMoveInputSource.swift

--- Sources/KonaneMoveInputSource.swift
+++ Sources/KonaneMoveInputSource.swift
@@ -4,16 +4,5 @@ class KonaneMoveInputSource {
     init(isBlack: Bool) {
         self.isBlack = isBlack
     }
-
-    func removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
-        fatalError("Override me!")
-    }
-
-    func removeSecondPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
-        fatalError("Override me!")
-    }
-
-    func nextMove(gameState: KonaneGameState) -> KonaneMove {
-        fatalError("Override me!")
-    }
+    //boi
 }
