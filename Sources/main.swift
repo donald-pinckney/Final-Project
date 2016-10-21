729f10322b567ec845f3126d0c16a67de681d443
niraqw15 <niraqw@gmail.com>
2016-10-21 04:05:50 +0000

    Added extremely basic and possibly wrong foundation.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,4 @@
 let game = KonaneGame(blackIsHuman: true, whiteIsHuman: true)
 print("Did black win? \(game.play())")
+
+//Make a file for each class/enum.
---
+++ Sources/tempClassesHolder.swift
@@ -0,0 +1,10 @@
+//Make each of these an inividual file.
+
+
+class KonaneGame {}
+class KonaneGameState {}
+enum KonaneColor {
+	case black, white, empty
+}
+class KonaneMove {}
+class KonaneMoveInputSource {}
