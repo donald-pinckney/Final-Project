ade1c1c03885073bee1a3f4c4360752f451ff982
Alex Hille <alex_hill20@icloud.com>
2016-10-21 04:03:16 +0000

    ASCII Art Gameboard real

--- Sources/KonaneGameState.swift
+++ Sources/KonaneGameState.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+
+/*
+
+
+
+15  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+14  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+13  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+12  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+11  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+10  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+9   o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+8   x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+7   o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+6   x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+5   o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+4   x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+3   o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+2   x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+1   o  x  o  x  o  x  o  x  o  x  o  x  o  x  o  x
+0   x  o  x  o  x  o  x  o  x  o  x  o  x  o  x  o
+
+    0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
+
+
+
+*/
