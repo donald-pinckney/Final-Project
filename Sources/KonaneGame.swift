e270b6bacc3923599692499d3dfb1cbe3e8e074d
Ionatan Soule <ionatan98@gmail.com>
2016-10-22 11:47:37 -0700

    Finished up code to initialize KonaneGame.swift with user input

--- Sources/KonaneGame.swift
+++ Sources/KonaneGame.swift
@@ -8,38 +8,66 @@
 
 import Foundation
 
-//Initialize settings for games: 
 /*
-    Initialize input sources
-    Choose which player goes first
+KonaneGame
+x   - init(blackIsHuman: Bool, whiteIsHuman: Bool)
+x   - private gameState: KonaneGameState
+    - private blackInputSource: KonaneMoveInputSource
+    - private whiteInputSource: KonaneMoveInputSource
+    - play() -> Bool // Returns true if black wins.
+    - private displayBoard()
 */
 
-print("Is player 1 a Human? Answer yes or no")
+class KonaneGame {
 
-if String(readLine()!) ==  "yes"{
-    print("Player 1 is a Human")
-        print("FATAL ERROR: NO CODE WRITEN YET")
-            //Add code for input source
-} else {
-    print("Player 1 is the Computer")
-        print("FATAL ERROR: NO CODE WRITEN YET")
-            //Add code for input source
+    private let blackIsHuman: Bool = true
+    private let whiteIsHuman: Bool = true
+    private var gameState: KonaneGameState = KonaneGameState.gameBoard
+
+    //BEGIN User input to initialize KonaneGame
+    private var initializeBlack: Bool = true
+    private var initializeWhite: Bool = true
+
+    initialize while true {
+        print("Is black human? [Y/N]:", terminator: " ")
+
+        if String(readLine()!) ==  "Y" {
+            print("Black is human")
+            initializeBlack = true
+        } 
+        else if String(readLine()!) ==  "N" {
+            print("Black is AI")
+            initializeBlack = false
+        } 
+        else {
+            print("Invalid. Please start over.")
+            repeat initialize
+        }
+        print("Is white human? [Y/N]")
+
+        if String(readLine()!) ==  "Y"{
+            print("White is human")
+            initializeWhite = true
+        } 
+        else if String(readLine()!) ==  "N" {
+            print("White is AI")
+            initializeWhite = false
+        } 
+        else {
+            print("Invalid. Please start over.")
+            repeat initialize
+        }
+        break initialize
+    }
+    //END User input to initialize KonaneGame
+
+    //Initialize KonaneGame with user input
+    init (blackIsHuman: initializeBlack, whiteIsHuman: initializeWhite)
+
+    print()
+
+
+    //write code for human playing
+    /* first step remomval of pieces
+    */
 }
-
-print("Is player 2 a Human? Answer yes or no")
-
-if String(readLine()!) ==  "yes"{
-    print("Player 2 is a Human")
-    print("FATAL ERROR: NO CODE WRITEN YET")
-            //Add code for input source
-} else {
-    print("Player 2 is the Computer")
-        print("FATAL ERROR: NO CODE WRITTEN YET")
-            //Add code for input source
-}
-
-print()
-
-//write code for human playing
-/* first step remomval of pieces
-*/
