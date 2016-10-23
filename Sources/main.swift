430e87cbbc7eebb34e95cf16a731f2c8055c74c1
Astha Sahoo <Astha Sahoo>
2016-10-23 12:40:51 -0700

    I made a couple of changes, mainly just testing it out, btw this is Astha :)

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,16 @@
-let game = KonaneGame(blackIsHuman: true, whiteIsHuman: true)
-print("Did black win? \(game.play())")
+class KonaneGame {
+	let game = KonaneGame(blackIsHuman: true, whiteIsHuman: true)
+		print("Did black win? \(game.play())")
+	init() {
+		var BlackIsHuman = Bool
+		var WhiteIsHuman = Bool
+		if BlackIsHuman == false {
+			WhiteIsHuman == true
+		} else {
+			BlackIsHuman == true
+		}
+	}
+	var private.gameState: KonaneGameState
+	let private.blackInputSource: KonaneMoveInputSource
+	let private.whiteInputSource: KonaneMoveInputSource
+}
