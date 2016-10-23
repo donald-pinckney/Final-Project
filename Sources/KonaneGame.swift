ec8667cc65a5c309eb8094df2171ee2852a2d75d
DevBurrito <jishnu1@gmail.com>
2016-10-23 10:05:35 -0700

    Added empty files for each class/enumerator

---
+++ Sources/KonaneGame.swift
@@ -0,0 +1,23 @@
+class KonaneGame {
+	private gameState: KonaneGameState
+	private blackInputSource: KonaneMoveInputSource
+	private whiteInputSource: KonaneMoveInputSource
+	init(blackIsHuman: Bool, whiteIsHuman: Bool) {
+		if blackIsHuman {
+			//black isn't AI
+		} else {
+			//black is AI
+		}
+		if whiteIsHuman {
+			//white isn't AI
+		} else {
+			//white is AI
+		}
+	}
+	func play() -> Bool {
+
+	}
+	private displayBoard() {
+
+	}
+}
