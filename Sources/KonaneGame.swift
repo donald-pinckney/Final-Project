f9ea3357e0a098edcf858f9bb3ee3959d720ba66
Gemma-B <gemma@bertain.net>
2016-10-25 02:45:13 +0000

    wrote skeleton code for KonaneGame as a class and all of it's states

---
+++ Sources/KonaneGame.swift
@@ -0,0 +1,10 @@
+class KonaneGame {
+        init(blackIsHuman: Bool, whiteIsHuman: Bool)
+        private gameState: KonaneGameState
+        private blackInputSource: KonaneMoveInputSource
+        private whiteInputSource: KonaneMoveInputSource
+        play() -> Bool // returns true if black wins
+        private displayBoard()
+}
+
+
