7cf7fb3219ce6661ed2a3cf1562192a426d2840e
Gemma-B <gemma@bertain.net>
2016-10-25 04:21:29 +0000

    I tried to turn some of the list of KonaneGameState into code

--- Sources/main.swift
+++ Sources/main.swift
@@ -28,10 +28,11 @@ class Konane {
 //Are the following functions? The ones that I pointed out down in the list????
 	func isValid(move: KonaneMove) -> Bool
 	func isValid(blackRemove: (x: Int, y: Int)) -> Bool {
-			if IsValid == false {
-			print(x, y + "is not valid")
-		} else {
-			print("The move is valid")
+	if IsValid == false {
+	    print(x, y + "is not valid")
+	    }
+        else {
+    	print("The move is valid")
 		}
 	}
 
@@ -48,23 +49,38 @@ class KonaneMove {
 	var fromY
 	var toX
 	var toY
-}
-//KonaneGameState
-	- width: Int = 16
-	- height: Int = 16
-	- init()
-	- private internal board data storage
-	- private isBlackTurn: Bool
-	- getIsBlackTurn() -> Bool 
-	- color(atX: Int, atY: Int) -> KonaneColor // Bottom left, 0-indexed
-	Are the following properties functions, cuz I'm not really sure...
-	- isValid(move: KonaneMove) -> Bool
-	- isValid(blackRemove: (x: Int, y: Int)) -> Bool
-	- isValid(whiteRemove: (x: Int, y: Int)) -> Bool
-	- perform(move: KonaneMove)
-	- perform(blackRemove: (x: Int, y: Int))
-	- perform(whiteRemove: (x: Int, y: Int))
-	- didBlackWin() -> Bool
-	- didWhiteWin() -> Bool
-	
+class KonaneGameState
+    let width: Int = 16
+    let height: Int = 16
+    var init()
+    private  InternalBoardDataStorage
+    /* the InternalBoardDataStorage wasn't working because I think internal by itself is a command so I changed it to be a name all together */
+    private var isBlackTurn: Bool
+    print(getIsBlackTurn)
+
+    /*getIsBlackTurn is a function that I'm calling print(color(atX: Int, atY:    Int)) */
+    
+    /* I think this is how to call a function with either print(function) or just saying the function with parameters but I'm not entirely sure */
+   
+   /* Bottom left, 0-indexed are the following properties functions, cuz I'm not really sure... */
+   
+   isValid(move: KonaneMove)  
+   isValid(blackRemove: (x: Int, y: Int))
+   isValid(whiteRemove: (x: Int, y: Int))
+   perform(move: KonaneMove)
+   perform(blackRemove: (x: Int, y: Int))                   
+   perform(whiteRemove: (x: Int, y: Int))
+   didBlackWin()
+   didWhiteWin()
+   /*I don't think you need to add what the function returns as ( ex. -> Bool) but I could be wrong*/
+
+
+
+
+
+
+
+
+
+
 >>>>>>> 0cd73873c1727dea7f9981186c76b6059b4e7d49
