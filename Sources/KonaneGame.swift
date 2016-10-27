c4a61c98b92b7e029bb470ed846e1483b4654b37
niraqw15 <niraqw@gmail.com>
2016-10-27 03:23:31 +0000

    Added board set up. Working = ?

--- Sources/KonaneGame.swift
+++ Sources/KonaneGame.swift
@@ -3,7 +3,7 @@ class KonaneGame {
 	private let whiteInputSource: KonaneMoveInputSource
 	func play() -> Bool {} // Returns true if black wins.
     private func displayBoard() {}
-    init(blackIsHuman: Bool, whiteIsHuman: Bool){
-		//Stuff goes here.
+    init(blackIsHuman: Bool, whiteIsHuman: Bool) {
+		//Stuff goes here. //For a reversed loop in printing, use in for y in (0..<height).reversed()
 	}
 }
