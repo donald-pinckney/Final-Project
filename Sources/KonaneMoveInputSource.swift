ea14115513568c3b87cd486e44db707060f90c15
millerja14 <jmmiller95616@gmail.com>
2016-10-26 23:13:17 -0400

    Editted functions in KonaneMoveInputSource

--- Sources/KonaneMoveInputSource.swift
+++ Sources/KonaneMoveInputSource.swift
@@ -12,7 +12,7 @@ import Foundation
 KonaneMoveInputSource
 - init(isBlack: Bool)
 - isBlack
-    - removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) (should override)
+- removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) (should override)
 - removeSecondPiece(gameState: KonaneGameState) -> (x: Int, y: Int) (should override)
 - nextMove(gameState: KonaneGameState) -> KonaneMove
 
@@ -30,14 +30,18 @@ class KonaneMoveInputSource {
 
 	func removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
 		//WRITE CODE
+		return(x: 1, y: 1)
 	}
 
 	func removeSecondPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
 		//WRITE CODE
+		return(x: 1, y: 1)
 	}
 
 	func nextMove(gameState: KonaneGameState) -> KonaneMove {
 		//WRITE CODE
+		var move = KonaneMove(fromX: 1, fromY: 1, toX: 1, toY: 1)
+		return(move)
 	}
 }
 
