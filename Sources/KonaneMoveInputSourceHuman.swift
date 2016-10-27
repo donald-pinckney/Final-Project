086f7bfe19b08082fea812908b38a7d9d6697deb
Ionatan Soule <ionatan98@gmail.com>
2016-10-26 21:03:22 -0700

    removeFirstPiece... code-ish

--- Sources/KonaneMoveInputSourceHuman.swift
+++ Sources/KonaneMoveInputSourceHuman.swift
@@ -8,4 +8,13 @@
 
 import Foundation
 
-class KonaneMoveInputSourceHuman: KonaneMoveInputSoure {}
+class KonaneMoveInputSourceHuman: KonaneMoveInputSoure {
+
+	override func removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
+		print("what is the first piece you'd like to remove?")
+		var xCoor: Int = readline(()!)!
+		var yCoor: Int = readline(()!)!
+		return (x: xCoor, y: yCoor)
+		// -> isValid(blackremove: ...)
+	}
+}
