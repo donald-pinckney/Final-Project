37d56844c804ff00351d661dd9b5123c8c33f386
asthasahoo <tennisastha16@gmail.com>
2016-10-27 02:45:29 +0000

    Random

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,13 @@
-let game = KonaneGame(blackIsHuman: true, whiteIsHuman: true)
-print("Did black win? \(game.play())")
+class KonaneGame {
+    let game = KonaneGame(blackIsHuman: true, whiteIsHuman: true)
+        print("Did black win? \(game.play())")
+    var private gameState: String
+    let private blackInputSource: Int
+    let private whiteInputSource: Int
+
+
+
+
+print("Did black win?\(game.play())")
+git --add
+
