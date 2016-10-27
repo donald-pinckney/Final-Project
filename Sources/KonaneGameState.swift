13dfe8ecce89087a36a5d84b556c61e58199bb70
LTaylor <darting.thoughts@gmail.com>
2016-10-26 19:08:14 -0700

    Update KonaneGameState.swift

--- Sources/KonaneGameState.swift
+++ Sources/KonaneGameState.swift
@@ -2,11 +2,14 @@ class KonaneGameState {
    let width = 16
    let height = 16
    
-   private let singleXLine = [KonaneColor](repeating: KonaneColor.empty, count: 16)
-   private let gameBoard = [KonaneColor](repeating: singleXLine, count: 16)
+   private let gameBoard: [[KonaneColor]]
 
    private var isBlackTurn: Bool = true
    
+   init() {
+      let singleXLine = [KonaneColor](repeating: KonaneColor.empty, count: height)
+      gameBoard = [[KonaneColor]](repeating: singleXLine, count: width)
+   }
    
    func getIsBlackTurn() -> Bool{ //because isBlackTurn is private lol
        if isBlackTurn == true{ //does this work????
