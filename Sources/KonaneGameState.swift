352ca6846c3594e8b2b0f67415f07791fdba26ff
JMY2001 <justinmyeung@gmail.com>
2016-10-27 01:56:28 +0000

    Defined needed stuff in KonaneGameState

--- Sources/KonaneGameState.swift
+++ Sources/KonaneGameState.swift
@@ -1,5 +1,17 @@
 class KonaneGameState {
   let width = 16
   let height = 16
-// add way to update board
-}
+  init()
+  private internal board data storage
+  private isBlackTurn: Bool
+  getIsBlackTurn() -> Bool
+  color(atX: Int, atY: Int) -> KonaneColor // Bottom left, 0-indexed
+  isValid(move: KonaneMove) -> Bool
+  isValid(blackRemove: (x: Int, y: Int)) -> Bool
+  isValid(whiteRemove: (x: Int, y: Int)) -> Bool
+  perform(move: KonaneMove)
+  perform(blackRemove: (x: Int, y: Int))
+  perform(whiteRemove: (x: Int, y: Int))
+  didBlackWin() -> Bool
+  didWhiteWin() -> Bool
+  }
