ddce786bac045fb727f3cef207481a195e07c074
javaraser <livytaylor47@gmail.com>
2016-10-27 03:43:49 +0000

    A sort of working board display Game and Gamestate

--- Sources/KonaneGame.swift
+++ Sources/KonaneGame.swift
@@ -22,8 +22,22 @@ class KonaneGame {
         return true
     }
 
-    private func displayboard(){
-        //it yeah
+    func displayboard(){
+        for indexY in 0..<gameState.width{//prints ROWS
+            if gameState.width - indexY > 9{
+                print("\(gameState.width - indexY)", terminator:"   ")//Terminator is 3 spaces; this prints the index of y wait what
+            }else{
+                print("\(gameState.width - indexY)", terminator:"    ")//Terminator is 4 space
+            }
+            for indexX in 0..<gameState.height{//prints the THINGS in the rows
+                if gameState.color(atX: indexX, atY: indexY) == KonaneColor.black{
+                    print("[X]", terminator:"")
+                }else{
+                    print("[ ]", terminator:"")
+                }   
+            }
+            print("")
+        }
     }
 
 }
--- Sources/KonaneGameState.swift
+++ Sources/KonaneGameState.swift
@@ -1,14 +1,14 @@
 class KonaneGameState {
-   let width = 16
-   let height = 16
+    let width = 16
+    let height = 16
    
-   private var gameBoard: [[KonaneColor]]
+    private var gameBoard: [[KonaneColor]]
 
-   private var isBlackTurn: Bool = true
+    private var isBlackTurn: Bool = true
    
-   init() {
-      let singleYLine = [KonaneColor](repeating: KonaneColor.empty, count: height)//this is a column
-      gameBoard = [[KonaneColor]](repeating: singleYLine, count: width)
+    init() {
+    let singleYLine = [KonaneColor](repeating: KonaneColor.empty, count: height)//this is a column
+    gameBoard = [[KonaneColor]](repeating: singleYLine, count: width)
         for x in 0..<gameBoard.count{//this creates the checkerboard pattern :D
             for y in 0..<singleYLine.count{
                 if x % 2 == 0 && y % 2 == 0 || x % 2 != 0 && y % 2 != 0 {
@@ -18,14 +18,17 @@ class KonaneGameState {
                 }
             }
         }
-   }
+    }
    
-   func getIsBlackTurn() -> Bool{ //because isBlackTurn is private lol
-       if isBlackTurn == true{ //does this work????
-           return true
-       }else{
-           return false
-       }
-   }
+    func getIsBlackTurn() -> Bool{ //because isBlackTurn is private lol
+        if isBlackTurn == true{ //does this work????
+            return true
+        }else{
+            return false
+        }
+    }
 
+    func color(atX x: Int, atY y: Int) -> KonaneColor{
+        return gameBoard[x][y]
+    }
 }
