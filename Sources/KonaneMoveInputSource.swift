91a60f5372238efb43f09c734573bb017c78d657
Alex Hille <alex_hill20@icloud.com>
2016-10-27 02:13:36 +0000

    More changes

--- Sources/KonaneMoveInputSource.swift
+++ Sources/KonaneMoveInputSource.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+/*
+KonaneMoveInputSource
+- init(isBlack: Bool)
+- isBlack
+    - removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) (should override)
+- removeSecondPiece(gameState: KonaneGameState) -> (x: Int, y: Int) (should override)
+- nextMove(gameState: KonaneGameState) -> KonaneMove
+
+Subclasses:
+KonaneMoveInputSourceHuman
+[TeamName]_KonaneMoveInputSourceAI
+*/
