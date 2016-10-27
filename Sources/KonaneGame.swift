53504f680391aba70e94b19698f72d173c062e40
JMY2001 <justinmyeung@gmail.com>
2016-10-27 03:45:25 +0000

    Prototyped input source classes/subclasses

--- Sources/KonaneGame.swift
+++ Sources/KonaneGame.swift
@@ -44,6 +44,6 @@ class KonaneGame {
 		}
 	}
 	private displayBoard() {
-
+		
 	}
 }
---
+++ Sources/KonaneMoveInputSourceHuman.swift
@@ -0,0 +1,13 @@
+class KonaneMoveInputSource {
+	init(isBlack: Bool)
+  isBlack
+	func removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
+
+  }
+	func removeSecondPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
+
+  }
+	func nextMove(gameState: KonaneGameState) -> KonaneMove {
+
+  }
+}
