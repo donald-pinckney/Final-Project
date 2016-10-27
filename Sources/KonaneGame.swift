9122a3b5983b0318a3607cc46dc849d455962f39
JMY2001 <justinmyeung@gmail.com>
2016-10-27 03:24:44 +0000

    finished play() function, defined enum

--- Sources/KonaneGame.swift
+++ Sources/KonaneGame.swift
@@ -15,9 +15,35 @@ class KonaneGame {
 		}
 	}
 	func play() -> Bool {
-
+		//black's first turn
+		print("Black needs to remove a piece anywhere on the board.")
+		displayBoard()
+		let blackRemoveLocation = blackInputSource.removeFirstPiece(gameState: gameState.copy())
+		gameState.perform(move: blackRemoveLocation)
+		//white's first turn
+		print("Now White should remove a piece adjacent to the one removed by Black!")
+		displayBoard()
+		let whiteRemoveLocation = whiteInputSource.removeSecondPiece(gameState: gameState.copy())
+		gameState.perform(move: whiteRemoveLocation)
+		//now the game will start
+		while gameState.didWhiteWin() == false && gameState.didBlackWin() == false {
+			let mainGameMove: KonaneMove
+			//to be inserted: stuff to collect the move:: WIP, Jishnu
+			//stage the move
+			if gameState.getIsBlackTurn() {
+				print("It's black's turn")
+				displayBoard()
+				mainGameMove = blackInputSource.nextMove(gameState: gameState.copy())
+			} else {
+				print("It's white's turn")
+				displayBoard()
+				mainGameMove = whiteInputSource.nextMove(gameState: gameState.copy())
+			}
+			//actually do it
+			gameState.perform(move: mainGameMove)
+		}
 	}
 	private displayBoard() {
-		
+
 	}
 }
