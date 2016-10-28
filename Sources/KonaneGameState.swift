0be15d11acf42ae61fc6012a8aa86b41d56e1449
Whyhow314 <whyhow3.141@gmail.com>
2016-10-28 03:43:29 +0000

    Added lots of comments

--- Sources/KonaneGameState.swift
+++ Sources/KonaneGameState.swift
@@ -61,15 +61,18 @@ class konaneGameState{
   //checks if a KonaneMove is legal
   func isLegal(move: KonaneMove) -> Bool {
     var moveIsLegal = true
+    //Is the move going to and from actual places
     if move.fromX < 0 || move.fromY < 0 || move.fromX > 15 || move.fromY > 15 || move.toX < 0 || move.toY < 0 || move.toX > 15 || move.toY > 15 {
       moveIsLegal = false
     }
+    //Is the move being made by the right player
     if isBlackTurn && (color(atX: move.fromX, atY: move.fromY) != black {
       moveIsLegal = false
     }
-    else if !isBlackTurn && (color(atX: move.fromX, atY: move.fromY) != white {
+    if !isBlackTurn && (color(atX: move.fromX, atY: move.fromY) != white {
       moveIsLegal = false
     }
+    //Note: this says which color's turn it is, which will be useful later
     var turnColor: KonaneColor = KonaneColor.empty
     if isBlackTurn {
       turnColor = KonaneColor.black
@@ -77,14 +80,18 @@ class konaneGameState{
     else {
       turnColor = KonaneColor.white
     }
+    //if it is moving in X and not Y
     if move.fromY == move.toY && move.fromX != move.toX {
+      //I need this if...else to make the for loop work, since I don't know which is bigger
       if move.toX > move.fromX {
         for xToCheck in (move.fromX + 1)...move.toX {
+          //all of the spaces that the player jumps to should be empty
           if (xToCheck - move.fromX) % 2 == 0 {
             if color(atX: xToCheck, atY: move.fromY) != KonaneColor.empty {
               moveIsLegal = false
             }
           }
+          //all of the spaces that the player jumps over should be the opposite color as the player
           else {
             if color(atX: xToCheck, atY: move.fromY) == empty || color(atX: xToCheck, atY: move.fromY) == turnColor {
               moveIsLegal = false
@@ -94,11 +101,13 @@ class konaneGameState{
       }
       else {
         for xToCheck in move.toX..<move.fromX {
+          //all of the spaces that the player jumps to should be empty
           if (move.fromX - xToCheck) % 2 == 0 {
             if color(atX: xToCheck, atY: move.fromY) != KonaneColor.empty {
               moveIsLegal = false
             }
           }
+          //all of the spaces that the player jumps over should be the opposite color as the player
           else {
             if color(atX: xToCheck, atY: move.fromY) == empty || color(atX: xToCheck, atY: move.fromY) == turnColor {
               moveIsLegal = false
@@ -107,14 +116,18 @@ class konaneGameState{
         }
       }
     }
+    //if it is moving in Y and not X
     else if move.fromX == move.toX && move.fromY != move.toY {
+      //I need this if...else to make the for loop work, since I don't know which is bigger
       if move.toY > move.fromY {
         for yToCheck in (move.fromY + 1)...move.toY {
+          //all of the spaces that the player jumps to should be empty
           if (yToCheck - move.fromY) % 2 == 0 {
             if color(atX: move.fromX, atY: yToCheck) != KonaneColor.empty {
               moveIsLegal = false
             }
           }
+          //all of the spaces that the player jumps over should be the opposite color as the player
           else {
             if color(atX: move.fromX, atY: yToCheck) == empty || color(atX: move.fromX, atY: yToCheck) == turnColor {
               moveIsLegal = false
@@ -124,11 +137,13 @@ class konaneGameState{
       }
       else {
         for yToCheck in move.toY..<move.fromY {
+          //all of the spaces that the player jumps to should be empty
           if (move.fromY - yToCheck) % 2 == 0 {
             if color(atX: move.fromX, atY: yToCheck) != KonaneColor.empty {
               moveIsLegal = false
             }
           }
+          //all of the spaces that the player jumps over should be the opposite color as the player
           else {
             if color(atX: move.fromX, atY: yToCheck) == empty || color(atX: move.fromX, atY: yToCheck) == turnColor {
               moveIsLegal = false
@@ -137,6 +152,7 @@ class konaneGameState{
         }
       }
     }
+    //the player can't move in both X and Y, or move in neither X nor Y
     else {
       moveIsLegal = false
     }
@@ -145,6 +161,7 @@ class konaneGameState{
 
   //checks if black can remove a piece at a location at the start of the game
   func isLegal(blackRemove: (x: Int, y: Int)) -> Bool {
+    //checks if it is one of the 4 positions that black can remove from
     if (blackRemove.x == 0 || blackRemove.x == 7 || blackRemove.x == 8 || blackRemove.x == 15) && blackRemove.x == blackRemove.y {
       return true
     }
@@ -159,6 +176,7 @@ class konaneGameState{
     var legality = false
     for xDif in distances {
       for yDif in distances {
+        //checks if any real space next to the location to remove the black piece is empty
         if (whiteRemove.x + xDif) > -1 && (whiteRemove.x + xDif) < 16 && (whiteRemove.y + yDif) > -1 && (whiteRemove.y + yDif) < 16 {
           if color(atX: whiteRemove.x + xDif, atY: whiteRemove.x + yDif) == KonaneColor.empty {
             legality = true
