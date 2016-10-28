c984910d559f16c0c754d08775b5dced7262d7c6
livyt <livytaylor47@gmail.com>
2016-10-28 03:57:09 +0000

    Started isValid for normal moves

--- Sources/KonaneGameState.swift
+++ Sources/KonaneGameState.swift
@@ -12,9 +12,9 @@ class KonaneGameState {
         for x in 0..<gameBoard.count{//this creates the checkerboard pattern :D
             for y in 0..<singleYLine.count{
                 if x % 2 == 0 && y % 2 == 0 || x % 2 != 0 && y % 2 != 0 {
-                    gameBoard[x][y] = KonaneColor.white
-                }else{
                     gameBoard[x][y] = KonaneColor.black
+                }else{
+                    gameBoard[x][y] = KonaneColor.white
                 }
             }
         }
@@ -30,5 +30,19 @@ class KonaneGameState {
 
     func color(atX x: Int, atY y: Int) -> KonaneColor{
         return gameBoard[x][y]
+    }
+    
+    func isValid(move: KonaneMove) -> Bool{ //checking validity of ANY move
+        var withinBounds = true
+        if move.toX < 0 || move.toY < 0 || move.fromX > 15 || move.fromY > 15 {
+            withinBounds = false
+        }
+        return withinBounds
+    }
+    func isValid(blackRemove: (x: Int, y: Int)) -> Bool{ //checking validity of white and black remove @ start
+        return true //AD SRU
+    }
+    func isValid(whiteRemove: (x: Int, y: Int)) -> Bool{
+        return true //ADD HE
     }
 }
