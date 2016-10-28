a722f8c196e7b5adb05b7d5070a997a68b687216
JMY2001 <justinmyeung@gmail.com>
2016-10-28 03:19:09 +0000

    Draftedvalid move check

--- Sources/KonaneGameState.swift
+++ Sources/KonaneGameState.swift
@@ -2,7 +2,7 @@ class KonaneGameState {
   let width = 16
   let height = 16
   private var board: [[KonaneColor]]
-  
+
   init() {
 
     var column = [KonaneColor](repeating: KonaneColor.empty, length: height)
@@ -30,11 +30,38 @@ class KonaneGameState {
   }
 
   func isValid(move: KonaneMove) -> Bool {
-    
+    // Check if selected piece is valid
+    if !(0 <= fromX < width) && !(0 <= fromY < height) {
+      return false
+    } else if {
+      // color matches turn color
+      return false
+    }
+
+    // Check if move is valid
+    if !(0 <= toX < width) && !(0 <= toY < height) {
+      return false
+    }
+
+    // Need to add code to check for moves that jump more than once
+    // Go over this section with fresh set of eyes
+    if !(fromX == toX + 2) || !(fromX == toX - 2) {
+      if (fromX == toX) {
+        if !(fromY == toY + 2) || !(fromY == toY - 2) {
+          return false
+        } else {
+          return true
+        }
+      } else {
+        return false
+      }
+    } else {
+      return true
+    }
   }
 
   func isValid(blackRemove: (x: Int, y: Int)) -> Bool {
-    
+
 
   }
 
