924c5b6ac0e48dcf8df6106d9dbc1bc876cf2fb5
gl3nnleblanc <gl3nnleblanc@gmail.com>
2016-10-30 05:49:44 -0700

    Clarified that all of KonaneMoveInputSource's methods are defined in its subclasses

--- Sources/KonaneMoveInputSource.swift
+++ Sources/KonaneMoveInputSource.swift
@@ -1,6 +1,6 @@
 //
 //  KonaneMoveInputSource.swift
-//  
+//
 //
 //  Created by Alex Hill on 10/20/16.
 //
@@ -27,21 +27,17 @@ class KonaneMoveInputSource {
 	init(isBlack: Bool) {
 		self.isBlack = isBlack
 	}
-
+		// Placeholder functions defined in subclasses
 	func removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
-		//WRITE CODE
 		return(x: 1, y: 1)
 	}
 
 	func removeSecondPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
-		//WRITE CODE
 		return(x: 1, y: 1)
 	}
 
 	func nextMove(gameState: KonaneGameState) -> KonaneMove {
-		//WRITE CODE
 		var move = KonaneMove(fromX: 1, fromY: 1, toX: 1, toY: 1)
 		return(move)
 	}
 }
-
