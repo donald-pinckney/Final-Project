d2ecfa8c145ebc45ddf174f32f7089b85741717c
DevBurrito <jishnu1@gmail.com>
2016-10-30 17:47:47 -0700

    completed human input source

--- Sources/KonaneMoveInputSourceHuman.swift
+++ Sources/KonaneMoveInputSourceHuman.swift
@@ -1,11 +1,40 @@
 class KonaneMoveInputSourceHuman: KonaneMoveInputSource {
 	override func removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
-		
+		while true {
+			print("Input x (black):")
+			let x = Int(readLine()!)!
+			print("Input y (black):")
+      let y = Int(readLine()!)!
+
+			if gameState.isValid(blackRemove: (x, y)) {
+				return (x, y)
+				break
+			}
+		}
   }
 	override func removeSecondPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
+		while true {
+			print("Input x (white):")
+			let x = Int(readLine()!)!
+			print("Input y (white):")
+      let y = Int(readLine()!)!
 
+			if gameState.isValid(whiteRemove: (x, y)) {
+				return (x, y)
+				break
+			}
+		}
   }
 	override func nextMove(gameState: KonaneGameState) -> KonaneMove {
+		print("x coordinate of the piece: ")
+		let pieceX = Int(readLine()!)!
+		print("y coordinate of the piece: ")
+		let pieceY = Int(readLine()!)!
+		print("x coordinate of the destination: ")
+		let destX = Int(readLine()!)!
+		print("y coordinate of the destination: ")
+		let destY = Int(readLine()!)!
 
+		return KonaneMove(fromX: pieceX, fromY: pieceY, toX: destX, toY: destX)
   }
 }
