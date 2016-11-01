360b6eb7878d87cac5a403fb691ec01c3a8ed631
niraqw15 <niraqw@gmail.com>
2016-11-01 05:28:09 +0000

    Added nearly complete printing function to KonaneGame, still needs to make an instance of GameState and use it though.

--- Sources/KonaneGame.swift
+++ Sources/KonaneGame.swift
@@ -1,29 +1,41 @@
 class KonaneGame {
+
+    //Important: Need to Create an instance of KonaneGameState!
+
 	private let blackInputSource: KonaneMoveInputSource
 	private let whiteInputSource: KonaneMoveInputSource
+	private let rowLine: String
 	func play() -> Bool {} // Returns true if black wins.
 	//Displays the game board.
     private func displayBoard() {
-    /*
-    	var rowSquares: [String]
-    	for yVal in 0..<KonaneGameState.height {
-    		rowSquares[yVal]
-	    	for xVal in 0..<KonaneGameState.width {
-	    		rowSquares[yVal] +=
-	    	}
-    	}
 
-    	//Loop that prints the board out.
-
+    	//Makes and prints each row of the board
     	for yVal in (0..<KonaneGameState.height).reversed() {
-    		print(horizontalLine)
-    		print(rowNum + horizontalSquares)
+    		private var currentRowSquares: String = "|"
+    		for xVal in 0..<KonaneGameState.width {
+    			//Turns the enum value into something the computer can print.
+    			if KonaneGameState.boardDataStorage[yVal][xVal] == KonaneColor.black {
+    				currentRowSquares += " X |"
+    			} else if KonaneGameState.boardDataStorage[yVal][xVal] == KonaneColor.white {
+    				currentRowSquares += " O |"
+    			} else {
+    				currentRowSquares += "   |"
+    			}
+    		}
+    		print(rowLine)
+    		print(currentRowSquares)
     	}
-    	print(spacesAmount + horizontalLine)
-    	print(columnNums)
-    	*/
-    }
+    	print(rowLine)
+   	}
+
     init(blackIsHuman: Bool, whiteIsHuman: Bool){
 		//Stuff goes here.
+
+
+		//Creates the horizontal line for printing the board.
+		private var rowLine: String = "+"
+		for xVal in 0..<KonaneGameState.width {
+    		rowLine += "---+"
+    	}
 	}
 }
