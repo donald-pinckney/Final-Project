b1578a50f6e133db9cf5b4bd9fbda770ed1eb3b7
niraqw15 <niraqw@gmail.com>
2016-11-04 03:48:15 +0000

    Made small improvement to the flow of removeFirstPiece.

--- Sources/KonaneMoveInputSourceHuman.swift
+++ Sources/KonaneMoveInputSourceHuman.swift
@@ -2,25 +2,23 @@ class KonaneMoveInputSourceHuman: KonaneMoveInputSource {
 	override func removeFirstPiece(gameState: KonaneGameState) -> (x: Int, y: Int) {
 
 		let gameState = KonaneGameState()
-		var rX: Int = 0
-		var rY: Int = 0
+		var x: Int = 0
+		var y: Int = 0
 		let loop = true
 		while loop {
 
 	    	print("Black: What piece do you want to remove?")
 	        print("X:")
-	        let x = Int(readLine()!)!
+	        x = Int(readLine()!)!
 	        print("Y:")
-	        let y = Int(readLine()!)!
+	        y = Int(readLine()!)!
 	        print()
 
 	        let isValid = gameState.isValid(blackRemove: (x: x, y: y))
 
 	        //Ensures that the move is valid before exiting the loop.
 	        if isValid {
-	        	rX = x
-	        	rY = y
-	        	return (rX, rY)
+	        	return (x, y)
 	        }
 	    }
     }
